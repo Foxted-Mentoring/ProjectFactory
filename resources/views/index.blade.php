114d5557fdab49d7f46789b18899552d579f5198
Valentin Prugnaud <valentin@whatdafox.com>
2015-10-18 20:44:40 -0700

    Basic design

---
+++ resources/views/index.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Projects Factory</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <i class="fa fa-refresh fa-3x"></i>
+                <div class="divider">
+                    <span>Project</span>
+                </div>
+                <h1>e-commerce website</h1>
+                <div class="columns">
+                    <div class="half">
+                        <div class="divider">
+                            <span>Backend</span>
+                        </div>
+                        <h2>Django</h2>
+                    </div>
+                    <div class="half">
+                        <div class="divider">
+                            <span>Frontend</span>
+                        </div>
+                        <h2>Meteor</h2>
+                    </div>
+                </div>
+                <small>
+                    <a href="https://github.com/foxted/ProjectFactory">Project Factory</a> by <a href="http://valentinprugnaud.com">Valentin Prugnaud</a>
+                </small>
+            </div>
+        </div>
+    </body>
+</html>
